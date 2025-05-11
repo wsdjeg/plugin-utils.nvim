21899e2e25ca144361ee11892573562011cc8a98
Eric Wong <wsdjeg@outlook.com>
2025-05-11 11:27:13 +0800

    update

--- lua/plugin-utils/data/string.lua
+++ lua/plugin-utils/data/string.lua
@@ -6,36 +6,25 @@
 -- License: GPLv3
 --=============================================================================
 
-
 local M = {}
 
-
 function M.trim(str)
-
-    return str:match( "^%s*(.-)%s*$" )
+    return str:match('^%s*(.-)%s*$')
 end
 
-function M.fill(str, length, ...)
-    local v = ''
-    local rightmost
-    if string.len(str) <= length then
-        v = str
-    else
-        rightmost= 0
-        while string.len(string.sub(str, 0, rightmost)) < length do
-            rightmost = rightmost + 1
-        end
-
+function M.fill(str, length, char)
+    local l = vim.fn.strdisplaywidth(str)
+    if l <= length then
+        return str .. string.rep(char or ' ', length - l)
     end
-    v = string.sub(str, 0, rightmost)
-    local char = select(1, ...) or ' '
-    return v .. string.rep(char, length - string.len(v))
+
+    return str
 end
 
 function M.strcharpart(str, start, _end)
     local chars = M.string2chars(str)
     local new_chars = {}
-    for i=start, math.min(_end, #chars) do
+    for i = start, math.min(_end, #chars) do
         table.insert(new_chars, chars[i])
     end
     return table.concat(new_chars, '')
@@ -56,14 +45,16 @@ function M.toggle_case(str)
     return table.concat(chars, '')
 end
 
-function M.fill_left(str)
-    
+function M.fill_left(str, length, char)
+    local l = vim.fn.strdisplaywidth(str)
+    if l <= length then
+        return string.rep(char or ' ', length - l) .. str
+    end
+
+    return str
 end
 
-function M.fill_middle(str, length, ...)
-    
-end
-
+function M.fill_middle(str, length, ...) end
 
 function M.trim_start(str)
     return str:gsub('^%s+', '')
@@ -75,53 +66,45 @@ end
 
 function M.string2chars(str)
     local list = {}
-    for uchar in string.gmatch(str, "[^\128-\191][\128-\191]*") do
+    for uchar in string.gmatch(str, '[^\128-\191][\128-\191]*') do
         table.insert(list, uchar)
     end
     return list
 end
 
 function M.matchstrpos(str, need, ...)
-  local matchedstr = vim.fn.matchstr(str, need, ...)
-  local matchbegin = vim.fn.match(str, need, ...)
-  local matchend = vim.fn.matchend(str, need, ...)
-  return {matchedstr, matchbegin, matchend}
-  -- return vim.fn.matchstrpos(str, need, ...)
+    local matchedstr = vim.fn.matchstr(str, need, ...)
+    local matchbegin = vim.fn.match(str, need, ...)
+    local matchend = vim.fn.matchend(str, need, ...)
+    return { matchedstr, matchbegin, matchend }
+    -- return vim.fn.matchstrpos(str, need, ...)
 end
 
 function M.strAllIndex(str, need, use_expr)
-  local rst = {}
-  if use_expr then
-    local idx = M.matchstrpos(str, need)
-    while idx[2] ~= -1 do
-      table.insert(rst, {idx[2], idx[3]})
-      idx = M.matchstrpos(str, need, idx[3])
+    local rst = {}
+    if use_expr then
+        local idx = M.matchstrpos(str, need)
+        while idx[2] ~= -1 do
+            table.insert(rst, { idx[2], idx[3] })
+            idx = M.matchstrpos(str, need, idx[3])
+        end
+    else
+        local pattern = [[\<\V]] .. need .. [[\ze\W\|\<\V]] .. need .. [[\ze\$]]
+        local idx = vim.fn.match(str, pattern)
+        while idx ~= -1 do
+            table.insert(rst, { idx, idx + vim.fn.len(need) })
+            idx = vim.fn.match(str, pattern, idx + 1 + vim.fn.len(need))
+        end
     end
-  else
-    local pattern = [[\<\V]] .. need .. [[\ze\W\|\<\V]] .. need .. [[\ze\$]]
-    local idx = vim.fn.match(str, pattern)
-    while idx ~= -1 do
-      table.insert(rst, {idx, idx + vim.fn.len(need)})
-      idx = vim.fn.match(str, pattern, idx + 1 + vim.fn.len(need))
-    end
-  end
-  return rst
+    return rst
 end
 
-function M.strQ2B(str)
-    
-end
+function M.strQ2B(str) end
 
-function M.strB2Q(str)
-    
-end
+function M.strB2Q(str) end
 
-function M.split(str, ...)
-    
-end
+function M.split(str, ...) end
 
 return M
 
-
 -- @todo add lua string api
-
